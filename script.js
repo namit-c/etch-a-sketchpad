9f75bdbf7f050d15e5a8c9970e1e2860019d0b93
Namit Chopra <namitc.11@gmail.com>
2020-09-05 22:27:49 -0400

    fixed the bug that removed the grid template columns attribute from grid class

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let gridContainer = document.querySelector(".grid");
-for(let i = 0; i < 16; i++){
-  gridContainer.style.gridTemplateColumns += " auto";
-}
+
 for(let i = 0; i < (16*16); i++){
   let gridBox = document.createElement("div");
   gridBox.classList.add("grid-box");
-  gridBox.style.cssText = "background-color: aqua; border: 1px solid black";
+  gridBox.style.cssText = "background-color: aqua; border: 1px solid white";
   gridContainer.append(gridBox);
 }
 
+for(let i = 0; i < 16; i++){
+    gridContainer.style.gridTemplateColumns += " auto";
+}
 
 gridContainer.addEventListener("mouseover", function(e){
-  e.target.style.cssText = "background-color: red";
+    //if statement to make sure the grid container does not get coloured red; only the divs inside it
+    if(e.target != gridContainer){
+        e.target.style.cssText += "background-color: red";
+    }
 });
